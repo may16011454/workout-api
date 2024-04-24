53fb4e70daa110a55d1291bece575652d4853a60
Logan <92788643+may16011454@users.noreply.github.com>
2024-04-24 15:55:20 +0100

    Add files via upload

---
+++ ToyShopAPI/Models/WorkoutModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutAPI.Models
+{
+    public class WorkoutModel
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        public int ActivityID { get; set; }
+
+        [ForeignKey("ActivityID")]
+        public ActivityModel? Activity { get; set; }
+
+        [Required]
+        public double? Duration { get; set; }
+
+    }
+}
---
+++ ToyShopAPI/README.md
@@ -0,0 +1,2 @@
+# WorkoutAPI
+
